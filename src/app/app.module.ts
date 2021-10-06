98f3bf678c6e937ab097e4f4cf62f8c96bb45c29
Florian Spier <mail@florian-spier.de>
2021-10-06 21:24:36 +0200

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DbService } from './api/db.service';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { TodoModule } from './modules/todo/todo.module';
+import { CounterModule } from './modules/counter/counter.module';
+import { StoreDevtoolsModule, StoreModule } from 'mini-rx-store-ng';
+import { ImmutableStateExtension, UndoExtension } from 'mini-rx-store';
+import { ProductStateModule } from './modules/products/state/product-state.module';
+import { UserModule } from './modules/user/user.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(DbService, { delay: 500, put204: false }),
+        AppRoutingModule,
+        TodoModule,
+        CounterModule,
+        UserModule,
+        StoreModule.forRoot({
+            extensions: [new ImmutableStateExtension(), new UndoExtension()],
+        }),
+        StoreDevtoolsModule.instrument({
+            name: 'Stackblitz Angular MiniRx Todos Showcase',
+            maxAge: 25,
+            latency: 250,
+        }),
+        ProductStateModule,
+    ],
+    declarations: [AppComponent],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/modules/products/products.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProductsRoutingModule } from './products-routing.module';
+import { ProductsShellComponent } from './components/products-shell/products-shell.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductFilterComponent } from './components/product-filter/product-filter.component';
+
+@NgModule({
+    declarations: [
+        ProductsShellComponent,
+        ProductDetailComponent,
+        ProductListComponent,
+        ProductFilterComponent,
+    ],
+    imports: [
+        CommonModule,
+        ProductsRoutingModule,
+        MatSidenavModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
+})
+export class ProductsModule {}
